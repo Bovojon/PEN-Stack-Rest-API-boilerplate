a763832dd34216f6f695271b92b1d32e9a7ad8d4
Bovojon <bovojon@gmail.com>
2020-10-17 23:20:08 -0700

    Remove index route

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
-import indexRouter from './routes/index.js';
 import userRouter from './routes/users.js';
 
 const app = express();
@@ -11,7 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
 app.use('/users', userRouter);
 
 export default app;
